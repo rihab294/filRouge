1459dbfd66e01f835e28c9d8ebcb158bb28cd580
rihab294 <rihabbet@gmail.com>
2024-12-03 17:04:54 +0100

    ajout des commentaires et rate

--- recipes_read.php
+++ recipes_read.php
@@ -16,15 +16,46 @@ if (!isset($getData['id']) || !is_numeric($getData['id'])) {
 }
 
 // On récupère la recette
-$retrieveRecipeStatement = $mysqlClient->prepare('SELECT r.* FROM recipes r WHERE r.recipe_id = :id ');
-$retrieveRecipeStatement->execute([
+$retrieveRecipeWithCommentsStatement = $mysqlClient->prepare('SELECT r.*, c.comment_id, c.comment, c.user_id,  DATE_FORMAT(c.created_at, "%d/%m/%Y") as comment_date, u.full_name FROM recipes r 
+LEFT JOIN comments c on c.recipe_id = r.recipe_id
+LEFT JOIN users u ON u.user_id = c.user_id
+WHERE r.recipe_id = :id 
+ORDER BY comment_date DESC');
+$retrieveRecipeWithCommentsStatement->execute([
     'id' => (int)$getData['id'],
 ]);
-$recipe = $retrieveRecipeStatement->fetch();
+$recipeWithComments = $retrieveRecipeWithCommentsStatement->fetchAll(PDO::FETCH_ASSOC);
 
-if (!$recipe) {
+if ($recipeWithComments === []) {
     echo('La recette n\'existe pas');
     return;
+}
+$retrieveAverageRatingStatement = $mysqlClient->prepare('SELECT ROUND(AVG(c.review),1) as rating FROM recipes r LEFT JOIN comments c on r.recipe_id = c.recipe_id WHERE r.recipe_id = :id');
+$retrieveAverageRatingStatement->execute([
+    'id' => (int)$getData['id'],
+]);
+$averageRating = $retrieveAverageRatingStatement->fetch();
+;
+
+$recipe = [
+    'recipe_id' => $recipeWithComments[0]['recipe_id'],
+    'title' => $recipeWithComments[0]['title'],
+    'recipe' => $recipeWithComments[0]['recipe'],
+    'author' => $recipeWithComments[0]['author'],
+    'comments' => [],
+    'rating' => $averageRating['rating'],
+];
+
+foreach ($recipeWithComments as $comment) {
+    if (!is_null($comment['comment_id'])) {
+        $recipe['comments'][] = [
+            'comment_id' => $comment['comment_id'],
+            'comment' => $comment['comment'],
+            'user_id' => (int) $comment['user_id'],
+            'full_name' => $comment['full_name'],
+            'created_at' => $comment['comment_date'],
+        ];
+    }
 }
 ?>
 
@@ -51,8 +82,34 @@ if (!$recipe) {
             </article>
             <aside class="col">
                 <p><i>Contribuée par <?php echo($recipe['author']); ?></i></p>
+                <?php if ($recipe['rating'] !== null) : ?>
+                    <p><b>Evaluée par la communauté à <?php echo($recipe['rating']); ?> / 5</b></p>
+                <?php else : ?>
+                    <p><b>Aucune évaluation</b></p>
+                <?php endif; ?>
             </aside>
         </div>
+        <hr />
+        <h2>Commentaires</h2>
+        <?php if ($recipe['comments'] !== []) : ?>
+        <div class="row">
+            <?php foreach ($recipe['comments'] as $comment) : ?>
+                <div class="comment">
+                    <p><?php echo($comment['created_at']); ?></p>
+                    <p><?php echo($comment['comment']); ?></p>
+                    <i>(<?php echo $comment['full_name']; ?>)</i>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <?php else : ?>
+        <div class="row">
+            <p>Aucun commentaire</p>
+        </div>
+        <?php endif; ?>
+        <hr />
+        <?php if (isset($_SESSION['LOGGED_USER'])) : ?>
+            <?php require_once(__DIR__ . '/comments_create.php'); ?>
+        <?php endif; ?>
     </div>
     <?php require_once(__DIR__ . '/footer.php'); ?>
 </body>
